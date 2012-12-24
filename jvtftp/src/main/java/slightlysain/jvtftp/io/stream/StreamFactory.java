6fb4f4519e2a1410123ff881f337e2aafcdd118b
Slightly Sain <slightlysain@gmail.com>
2012-12-24 00:09:07 +0000

    Stream factory and JvtftpInput
    New classes to handle stream creation and the management of piped
    streams

---
+++ jvtftp/src/main/java/slightlysain/jvtftp/io/stream/StreamFactory.java
@@ -0,0 +1,21 @@
+package slightlysain.jvtftp.io.stream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface StreamFactory {
+
+	public abstract InputStream getFileInputStream(String filename)
+			throws FileNotFoundException;
+
+	public abstract OutputStream getFileOutputStream(String filename)
+			throws FileNotFoundException;
+	
+	public abstract File getOutputFile(String filename);
+	
+	public abstract File getInputFile(String filename);
+
+
+}
---
+++ jvtftp/src/main/java/slightlysain/jvtftp/io/stream/StreamFactoryImpl.java
@@ -0,0 +1,58 @@
+package slightlysain.jvtftp.io.stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamFactoryImpl implements StreamFactory {
+
+	private String incomingDir;
+	private String outgoingDir;
+
+	public StreamFactoryImpl(String incomingDir, String outgoingDir) {
+		this.incomingDir = incomingDir;
+		this.outgoingDir = outgoingDir;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * slightlysain.jvtftp.streamfactory.StreamFactory#getFileInputStream(java
+	 * .lang.String)
+	 */
+	public InputStream getFileInputStream(String filename)
+			throws FileNotFoundException {
+		FileInputStream fis = new FileInputStream(outgoingDir
+				+ File.separatorChar + filename);
+		return fis;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * slightlysain.jvtftp.streamfactory.StreamFactory#getFileOutputStream(java
+	 * .lang.String)
+	 */
+	public OutputStream getFileOutputStream(String filename)
+			throws FileNotFoundException {
+		FileOutputStream fos = new FileOutputStream(incomingDir
+				+ File.separatorChar + filename);
+		return fos;
+	}
+
+	public File getOutputFile(String filename) {
+		File file = new File(incomingDir + File.separatorChar + filename);
+		return file;
+	}
+
+	public File getInputFile(String filename) {
+		File file = new File(outgoingDir + File.separatorChar + filename);
+		return file;
+	}
+
+}
